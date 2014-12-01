e45a1a9b4e915f11b01f04e818b7df51172b2d51
Rafael Vidaurre <narzerus@gmail.com>
2014-12-01 18:47:26 -0300

    Fix some tests and prepare example files

--- example/example.js
+++ example/example.js
@@ -1,1 +1,28 @@
+'use strict';
+
 var Yakuza = require('../yakuza');
+
+// Create scraper
+Yakuza.scraper('Articles');
+
+// Create agents
+Yakuza.scraper('Articles').agent('Reddit')
+  .setup(function (config) {
+    config.executionPlan = [
+      'getArticleLinks',
+      'getArticles'
+    ];
+  });
+
+// Create tasks
+Yakuza.scraper('Articles').agent('Reddit').task('getArticleLinks')
+  .main(function (emitter, http, params) {
+    http.get('http://www.reddit.com/', function (err, res, body) {
+      console.log(body);
+    });
+  });
+
+
+var job = Yakuza.job('Articles', 'Reddit');
+
+job.enqueue('getArticleLinks');
