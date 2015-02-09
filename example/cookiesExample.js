5a27aaf877a81548141a10366e4830dc431d844d
Rafael Vidaurre <narzerus@gmail.com>
2015-02-09 15:13:47 -0300

    Finish cookie support and cookie example

--- example/cookiesExample.js
+++ example/cookiesExample.js
@@ -4,7 +4,7 @@ var Yakuza = require('../yakuza');
 var _ = require('lodash');
 var cheerio = require('cheerio');
 
-Yakuza.scoper('Loginer');
+Yakuza.scraper('Loginer');
 
 Yakuza.scraper('Loginer').agent('Santander')
   .setup(function (config) {
@@ -14,17 +14,16 @@ Yakuza.scraper('Loginer').agent('Santander')
     ];
   });
 
-Yakuza.scraper('Loginer').agent('Santander').task('login')
+Yakuza.task('Loginer', 'Santander', 'login')
   .builder(function (job) {
     return job.params.credentials;
   })
 
   .main(function (task, http, params) {
-    var username, password, baseUrl, opts;
+    var username, password, baseUrl, opts, loginData;
 
-
-    username = params.credentials.username;
-    password = params.credentials.password;
+    username = params.username;
+    password = params.password;
     baseUrl = 'https://www.santander.cl/transa/cruce.asp';
 
     loginData = {
@@ -53,7 +52,6 @@ Yakuza.scraper('Loginer').agent('Santander').task('login')
     };
 
     http.post(opts, function (err, res, body) {
-      opts = sb.http.options();
       task.share('url', res.headers.location);
       task.saveCookies();
       task.success('Authorized');
@@ -61,12 +59,34 @@ Yakuza.scraper('Loginer').agent('Santander').task('login')
   });
 
 Yakuza.task('Loginer', 'Santander', 'afterLogin')
+  .builder(function () {
+    console.log(true);
+  })
+  .main(function (task, http, params) {
+    console.log('Jar received in afterLogin jar is: ');
+    console.log(JSON.stringify(http.getCookieJar()));
+
+    task.success();
+  });
 
 var jobParams = {
-  {credentials: {
+  credentials: {
     username: '15.311.907-4',
-    password: '6958'}
+    password: '6958'
   }
 };
 
-var loginTestJob = Yakuza.job('Loginer', 'Santander', jobParams});
+var loginTestJob = Yakuza.job('Loginer', 'Santander', jobParams);
+
+loginTestJob.enqueue('login');
+loginTestJob.enqueue('afterLogin');
+
+loginTestJob.on('fail', function (response) {
+  console.log('Job Failed!');
+});
+
+loginTestJob.on('success', function (response) {
+  console.log('Job Succeeded!');
+})
+
+loginTestJob.run();
