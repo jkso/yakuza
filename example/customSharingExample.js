8cb789ee38dbe9eef7f7e68982c5df4723e06fa2
Rafael Vidaurre <narzerus@gmail.com>
2015-02-12 18:54:28 -0300

    Custom sharing methods working

--- example/customSharingExample.js
+++ example/customSharingExample.js
@@ -5,7 +5,7 @@ var Yakuza = require('../yakuza');
 Yakuza.scraper('TestScraper').agent('TestAgent').setup(function (config) {
   config.plan = [
     'CountVocals',
-    'Task2'
+    'ShowResults'
   ];
 });
 
@@ -18,7 +18,7 @@ var sumProperties = function (current, next) {
     _.each(_.keys(current), function (key) {
       var currentValue = current[key];
       var nextValue = next[key] || 0;
-      newObjext[key] = currentValue + nextValue;
+      newObject[key] = currentValue + nextValue;
     });
   } else {
     newObject = next;
@@ -27,20 +27,22 @@ var sumProperties = function (current, next) {
   return newObject;
 };
 
-// Define a global sharing method. `current` refers to the actual value of the key in which we
-// are sharing. `Next` is the value we will now share in the key
+// Define a scraper-level sharing method. `current` refers to the actual value of the key in which
+// we are sharing. `Next` is the value we will now share in the key
 // The returned value will be the new value for the defined key
 // In this case the sumProperties method will sum the values of all the properties present in
 // the current object. For simplicity we are avoiding corner cases
-Yakuza.addShareMethod('sumProperties', sumProperties);
+Yakuza.scraper('TestScraper').addShareMethod('sumProperties', sumProperties);
 
-Yakuza.task('TestScraper', 'TestAgent', 'JoinStrings')
+Yakuza.task('TestScraper', 'TestAgent', 'CountVocals')
   .builder(function (job) {
     // Instance 'JoinStrings' once per element in params
     return job.params;
   })
   .main(function (task, http, params) {
     var counts, string;
+
+    console.log('Hi');
 
     string = params;
     counts = {a: 0, e: 0, i: 0, o: 0, u: 0};
@@ -55,4 +57,21 @@ Yakuza.task('TestScraper', 'TestAgent', 'JoinStrings')
 
     // Alternatively you could define the method here, which would only work for this task
     // task.share('counts', counts, {method: sumProperties});
+
+    task.success();
   });
+
+Yakuza.task('TestScraper', 'TestAgent', 'ShowResults')
+  .builder(function (job) {
+    return job.shared('CountVocals.counts');
+  })
+  .main(function (task, http, params) {
+    console.log(params);
+    task.success();
+  });
+
+var job = Yakuza.job('TestScraper', 'TestAgent', ['murcielago', 'ballena', 'canguro']);
+
+job.enqueue('CountVocals').enqueue('ShowResults');
+
+job.run();
